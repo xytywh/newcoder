9280a34a492db9d7683190ba9e29e63906cbe19c
gehui <15555387016gehui@gmail.com>
2022-08-21 14:09:41 +0800

    update 字节序

--- lesson31/byteorder.c
+++ lesson31/byteorder.c
@@ -15,10 +15,15 @@ int main() {
     } test;
 
     test.value = 0x0102;
+    // bytes[0]是内存低地址 01是数据高位字节
     if ((test.bytes[0] == 1) && (test.bytes[1] == 2)) {
         printf("大端字节序\n");
+        // 打印的结果是201
+        printf("test.values=%x\n", test.value);
     } else if ((test.bytes[0] == 2) && (test.bytes[1] == 1)) {
         printf("小端字节序\n");
+        // 打印的结果是102   对于数组来说 bytes[0]是低位地址 bytes[1]是高位地址  而对于values来说 左边是高位地址 右边是地位地址
+        printf("test.values=%x\n", test.value);
     } else {
         printf("未知\n");
     }
--- lesson31/bytetrans.c
+++ lesson31/bytetrans.c
@@ -18,32 +18,32 @@
 
 int main() {
 
-    // htons 转换端口
+    // htons 转换端口(端口是2个字节)
     unsigned short a = 0x0102;
-    printf("a : %x\n", a);
+    printf("a : %0x\n", a);
     unsigned short b = htons(a);
-    printf("b : %x\n", b);
+    printf("b : %0x\n", b);
 
     printf("=======================\n");
 
     // htonl  转换IP
     char buf[4] = {192, 168, 1, 100};
-    int num = *(int *)buf;
+    int num = *(int *) buf;
     int sum = htonl(num);
-    unsigned char *p = (char *)&sum;
+    unsigned char *p = (char *) &sum;
 
-    printf("%d %d %d %d\n", *p, *(p+1), *(p+2), *(p+3));
+    printf("%d %d %d %d\n", *p, *(p + 1), *(p + 2), *(p + 3));
 
     printf("=======================\n");
 
     // ntohl
     unsigned char buf1[4] = {1, 1, 168, 192};
-    int num1 = *(int *)buf1;
+    int num1 = *(int *) buf1;
     int sum1 = ntohl(num1);
-    unsigned char *p1 = (unsigned char *)&sum1;
-    printf("%d %d %d %d\n", *p1, *(p1+1), *(p1+2), *(p1+3));
-    
-     // ntohs
+    unsigned char *p1 = (unsigned char *) &sum1;
+    printf("%d %d %d %d\n", *p1, *(p1 + 1), *(p1 + 2), *(p1 + 3));
+
+    // ntohs
 
 
     return 0;
