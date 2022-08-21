a7cd7950b80f5a58cf8610be2769dac89cce8e6f
gehui <15555387016gehui@gmail.com>
2022-08-21 11:27:11 +0800

    网络通信过程

--- lesson31/byteorder.c
+++ lesson31/byteorder.c
@@ -15,9 +15,9 @@ int main() {
     } test;
 
     test.value = 0x0102;
-    if((test.bytes[0] == 1) && (test.bytes[1] == 2)) {
+    if ((test.bytes[0] == 1) && (test.bytes[1] == 2)) {
         printf("大端字节序\n");
-    } else if((test.bytes[0] == 2) && (test.bytes[1] == 1)) {
+    } else if ((test.bytes[0] == 2) && (test.bytes[1] == 1)) {
         printf("小端字节序\n");
     } else {
         printf("未知\n");
